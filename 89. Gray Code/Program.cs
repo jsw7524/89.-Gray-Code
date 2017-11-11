b01ac31c43f427d86b658b46ed75540fca63cbfb
Jason Wu <jsw7524@gmail.com>
2017-11-11 19:08:47 +0800

    ad-hoc

--- 89. Gray Code/Program.cs
+++ 89. Gray Code/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace _89.Gray_Code
 {
+    public class Solution
+    {
+        private List<string> GrayCodeList { get; set; } = new List < string >{"0", "1"};
+        public IList<int> GrayCode(int n)
+        {
+            if (n == 0)
+            {
+                return new List<int>(){0};
+            }
+            for (int i = 1; i < n; i++)
+            {
+                var tmp=GrayCodeList.Select(g => '0'+g).ToList();
+                GrayCodeList.Reverse();
+                GrayCodeList=tmp.Concat(GrayCodeList.Select(g => '1' + g)).ToList();
+            }
+            return GrayCodeList.Select(g => Convert.ToInt32(g, 2)).ToList();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var checker=(new Solution()).GrayCode(3);
         }
     }
 }
